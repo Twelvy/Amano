e4409f446a73231990e125c57e69f33ee5b04387
Aurélien Sérandour <Twelvy@users.noreply.github.com>
2020-09-30 09:53:54 +0900

    Rotate the camera instead of the model

--- Amano/Amano/Application.cpp
+++ Amano/Amano/Application.cpp
@@ -374,8 +374,8 @@ void Application::updateUniformBuffer() {
 	float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 	
 	PerFrameUniformBufferObject ubo{};
-	ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-	ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	ubo.view = glm::lookAt(glm::vec3(2.8f * cosf(time), 2.8f * sinf(time), 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 	ubo.proj = glm::perspective(glm::radians(45.0f), m_width / (float)m_height, 0.1f, 10.0f);
 
 	// TODO: fix this
