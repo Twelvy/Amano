da898522e50200f1e7b5966531f97cc141cd0365
Aurélien Sérandour <Twelvy@users.noreply.github.com>
2020-10-25 20:51:01 +0900

    Compile all the shaders in the shader directory.
    No need to list them manually

--- scripts/compile_shaders.bat
+++ scripts/compile_shaders.bat
@@ -1,20 +1,35 @@
+@echo off
+
 SET VULKAN_SDK=C:/VulkanSDK/1.2.135.0
 SET VULKAN_SDK_SPV_COMPILER=%VULKAN_SDK%/Bin/glslc.exe
 SET VULKAN_SDK_GLSLLANG=%VULKAN_SDK%/Bin/glslangValidator.exe
+SET OUTPUT_DIR=../compiled_shaders
 
-%VULKAN_SDK_SPV_COMPILER% ../shaders/gbuffer.vert -o ../compiled_shaders/gbufferv.spv
-%VULKAN_SDK_SPV_COMPILER% ../shaders/gbuffer.frag -o ../compiled_shaders/gbufferf.spv
+cd ../shaders/
 
-%VULKAN_SDK_SPV_COMPILER% ../shaders/deferred_lighting.comp -o ../compiled_shaders/deferred_lighting.spv
-
-%VULKAN_SDK_SPV_COMPILER% ../shaders/tonemapping.comp -o ../compiled_shaders/tonemapping.spv
-
-%VULKAN_SDK_GLSLLANG% -V ../shaders/raygen.rgen      -o ../compiled_shaders/raygen.spv
-%VULKAN_SDK_GLSLLANG% -V ../shaders/closesthit.rchit -o ../compiled_shaders/closesthit.spv
-%VULKAN_SDK_GLSLLANG% -V ../shaders/miss.rmiss       -o ../compiled_shaders/miss.spv
-
-%VULKAN_SDK_GLSLLANG% -V ../shaders/shadow_raygen.rgen      -o ../compiled_shaders/shadow_raygen.spv
-%VULKAN_SDK_GLSLLANG% -V ../shaders/shadow_closesthit.rchit -o ../compiled_shaders/shadow_closesthit.spv
-%VULKAN_SDK_GLSLLANG% -V ../shaders/shadow_miss.rmiss       -o ../compiled_shaders/shadow_miss.spv
-
-pause
+FOR /R %%i IN (*) DO (
+    IF %%~xi==.vert (
+        echo compiling vertex shader %%i
+        %VULKAN_SDK_SPV_COMPILER% %%i -o %OUTPUT_DIR%/%%~niv.spv
+    ) ELSE IF %%~xi==.frag (
+        echo compiling fragment shader %%i
+        %VULKAN_SDK_SPV_COMPILER% %%i -o %OUTPUT_DIR%/%%~nif.spv
+    ) ELSE IF %%~xi==.comp (
+        echo compiling compute shader %%i
+        %VULKAN_SDK_SPV_COMPILER% %%i -o %OUTPUT_DIR%/%%~ni.spv
+    ) ELSE IF %%~xi==.rgen (
+        echo compiling raygen shader %%i
+        %VULKAN_SDK_GLSLLANG% -V %%i -o %OUTPUT_DIR%/%%~ni.spv
+    ) ELSE IF %%~xi==.rchit (
+        echo compiling closest hit shader %%i
+        %VULKAN_SDK_GLSLLANG% -V %%i -o %OUTPUT_DIR%/%%~ni.spv
+    ) ELSE IF %%~xi==.rahit (
+        echo compiling any hit shader %%i
+        %VULKAN_SDK_GLSLLANG% -V %%i -o %OUTPUT_DIR%/%%~ni.spv
+    ) ELSE IF %%~xi==.rmiss (
+        echo compiling miss shader %%i
+        %VULKAN_SDK_GLSLLANG% -V %%i -o %OUTPUT_DIR%/%%~ni.spv
+    ) ELSE (
+        echo shader %%i is from an unknown/unsupported type
+    )
+)
